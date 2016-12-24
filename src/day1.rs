c6542f35e0344b957adaaa04f2968d6f9256cea9
Tom Gibson <beyamor@gmail.com>
2016-12-24 13:47:39 -0800

    Parsing moves, calculating distance

--- src/day1.rs
+++ src/day1.rs
@@ -18,11 +18,13 @@ pub struct State {
     direction: Direction
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub enum Turn {
     Left,
     Right
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub struct Move {
     turn: Turn,
     blocks: i32
@@ -83,6 +85,51 @@ pub fn make_moves(initial_state:&State, movs:&Vec<Move>) -> State {
     return movs.iter().fold(*initial_state, |state, mov| { make_move(&state, &mov) });
 }
 
+pub fn manhattan_distance(p1:&Point, p2:&Point) -> i32 {
+    return (p2.x - p1.x).abs() + (p2.y - p1.y).abs();
+}
+
+pub fn calculate_distance(movs:&Vec<Move>) -> i32 {
+    let start = State {
+        coordinates: Point { x: 0, y: 0 },
+        direction: Direction::North
+    };
+
+    let end = make_moves(&start, movs);
+
+    return manhattan_distance(&start.coordinates, &end.coordinates);
+}
+
+pub fn parse_move(s:&String) -> Result<Move, String> {
+    let mut chars = s.chars();
+
+    let turn = match chars.next() {
+        Some('L') => Turn::Left,
+        Some('R') => Turn::Right,
+        _ => return Err("Couldn't parse turn from: ".to_string() + s)
+    };
+
+    let blocks = match chars.collect::<String>().parse::<i32>() {
+        Ok(val) => val,
+        Err(s) => return Err(s.to_string())
+    };
+
+
+    return Ok(Move { turn: turn, blocks: blocks });
+}
+
+pub fn parse_moves(s:&String) -> Result<Vec<Move>, String> {
+    let tokens = s.trim().split(", ");
+    let mut moves = vec![];
+    for token in tokens {
+        match parse_move(&token.to_string()) {
+            Ok(mov) => moves.push(mov),
+            Err(e) => return Err(e)
+        }
+    }
+    return Ok(moves);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -127,5 +174,30 @@ mod tests {
             coordinates: Point { x: 10, y: -10 },
             direction: Direction::South
         });
+    }
+
+    #[test]
+    fn parsing_moves() {
+        assert_eq!(
+            parse_moves(&"R2, L3".to_string()).unwrap(),
+            vec![
+                Move { turn: Turn::Right, blocks: 2 },
+                Move { turn: Turn::Left, blocks: 3}
+            ]);
+    }
+
+    #[test]
+    fn test_examples() {
+        assert_eq!(
+            calculate_distance(&parse_moves(&"R2, L3".to_string()).unwrap()),
+            5);
+
+        assert_eq!(
+            calculate_distance(&parse_moves(&"R2, R2, R2".to_string()).unwrap()),
+            2);
+
+        assert_eq!(
+            calculate_distance(&parse_moves(&"R5, L5, R5, R3".to_string()).unwrap()),
+            12);
     }
 }
